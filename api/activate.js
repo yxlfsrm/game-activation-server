ae6c5f515c49de4f7e102c2079b54db0ed3045df
yxlfsrm <yxlfsrm@gmail.com>
2026-02-07 13:28:12 +0800

    更新 activate.js

--- api/activate.js
+++ api/activate.js
@@ -1,16 +1,26 @@
-let validCodes = [
-    "12345678901234567890",
-    "23456789012345678901",
-    "34567890123456789012"
-];
-module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    const { code } = req.query;
-    const index = validCodes.indexOf(code);
-    if (index === -1) {
-        return res.json({ ok: false, error: '激活码无效' });
-    } else {
-        validCodes.splice(index, 1);
-        return res.json({ ok: true, message: '激活成功' });
-    }
+const fs = require('fs');
+module.exports = async(req, res) = >
+{
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	const code = req.query.code;
+	const sourceFile = process.cwd() + '/codes.txt';
+	const packetFile = '/tmp/game.dat';
+	let data = '';
+	if (fs.existsSync(packetFile))
+		data = fs.readFileSync(packetFile, 'utf8');
+	else
+		data = fs.readFileSync(sourceFile, 'utf8');
+	const codes = data.split('\n').filter(c = >c.length == = 20);
+	const idx = codes.indexOf(code);
+	if (idx == = -1)
+		return res.json(
+						   {
+  ok: false, error:	   '激活码无效'}
+	);
+	codes.splice(idx, 1);
+	fs.writeFileSync(packetFile, codes.join('\n'));
+	return res.json(
+					   {
+  ok:				   true}
+	);
 };
