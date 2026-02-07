4de72d452559f630596ce4d30fe95172a57c7f65
yxlfsrm <yxlfsrm@gmail.com>
2026-02-07 12:12:14 +0800

    Refactor activation code handling in activate.js

--- api/activate.js
+++ api/activate.js
@@ -1,20 +1,15 @@
-// api/check.js - 最简单GET版本
 let validCodes = [
     "12345678901234567890",
     "23456789012345678901",
     "34567890123456789012"
 ];
 module.exports = async (req, res) => {
-    // 允许所有访问
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // 从URL参数获取激活码
-    const { code } = req.query; // GET请求用req.query
-    // 检查激活码
+    const { code } = req.query;
     const index = validCodes.indexOf(code);
     if (index === -1) {
         return res.json({ ok: false, error: '激活码无效' });
     } else {
-        // 删除已使用的激活码
         validCodes.splice(index, 1);
         return res.json({ ok: true, message: '激活成功' });
     }
